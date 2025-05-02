46680ac52b68e24d0ffa66dbb440aca091903bd9
Oleg Bilovus <68842946+olegbilovus@users.noreply.github.com>
2025-05-02 18:48:07 +0200

    Add oneDPL support for any_of benchmarks

--- include/pstl/benchmarks/any_of/group.h
+++ include/pstl/benchmarks/any_of/group.h
@@ -10,6 +10,10 @@
 #include "any_of_hpx.h"
 #endif
 
+#ifdef PSTL_BENCH_USE_ONE_DPL
+#include "any_of_one_dpl.h"
+#endif
+
 //region any_of_std
 template<class Policy>
 static void any_of_std_wrapper(benchmark::State & state)
@@ -49,9 +53,31 @@ static void any_of_hpx_wrapper(benchmark::State & state)
 #endif
 //endregion any_of_hpx
 
+//region any_of_one_dpl
+#ifdef PSTL_BENCH_USE_ONE_DPL
+template<class Policy>
+static void any_of_one_dpl_wrapper(benchmark::State & state)
+{
+	benchmark_any_of::benchmark_wrapper<Policy>(state, benchmark_any_of::any_of_one_dpl);
+}
+
+/*
+the std policy is just a placeholder, it will use oneapi::dpl::execution::dpcpp_default when executing the algorithm. 
+Check the algorithm implementation.
+*/
+#define ANY_OF_ONE_DPL_WRAPPER                                                               \
+	BENCHMARK_TEMPLATE1(any_of_one_dpl_wrapper, std::execution::parallel_unsequenced_policy) \
+	    ->Name(PSTL_BENCH_BENCHMARK_NAME("oneDPL::any_of"))                                  \
+	    ->PSTL_BENCH_BENCHMARK_PARAMETERS
+#else
+#define ANY_OF_ONE_DPL_WRAPPER
+#endif
+//endregion any_of_one_dpl
+
 #define ANY_OF_GROUP   \
 	ANY_OF_SEQ_WRAPPER \
 	ANY_OF_STD_WRAPPER \
-	ANY_OF_HPX_WRAPPER
+	ANY_OF_HPX_WRAPPER \
+	ANY_OF_ONE_DPL_WRAPPER
 
 ANY_OF_GROUP
