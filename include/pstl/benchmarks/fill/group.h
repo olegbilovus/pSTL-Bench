9c88ec18b8ca28bd62a46bd86254f024c4118a55
Oleg Bilovus <68842946+olegbilovus@users.noreply.github.com>
2025-05-02 20:47:49 +0200

    Add oneDPL support for fill benchmarks

--- include/pstl/benchmarks/fill/group.h
+++ include/pstl/benchmarks/fill/group.h
@@ -10,6 +10,10 @@
 #include "fill_hpx.h"
 #endif
 
+#ifdef PSTL_BENCH_USE_ONE_DPL
+#include "fill_one_dpl.h"
+#endif
+
 //region fill_std
 template<class Policy>
 static void fill_std_wrapper(benchmark::State & state)
@@ -49,9 +53,31 @@ static void fill_hpx_wrapper(benchmark::State & state)
 #endif
 //endregion fill_hpx
 
+//region fill_one_dpl
+#ifdef PSTL_BENCH_USE_ONE_DPL
+template<class Policy>
+static void fill_one_dpl_wrapper(benchmark::State & state)
+{
+	benchmark_fill::benchmark_wrapper<Policy>(state, benchmark_fill::fill_one_dpl);
+}
+
+/*
+the std policy is just a placeholder, it will use oneapi::dpl::execution::dpcpp_default when executing the algorithm. 
+Check the algorithm implementation.
+*/
+#define FILL_ONE_DPL_WRAPPER                                                               \
+	BENCHMARK_TEMPLATE1(fill_one_dpl_wrapper, std::execution::parallel_unsequenced_policy) \
+	    ->Name(PSTL_BENCH_BENCHMARK_NAME("oneDPL::fill"))                                  \
+	    ->PSTL_BENCH_BENCHMARK_PARAMETERS
+#else
+#define FILL_ONE_DPL_WRAPPER
+#endif
+//endregion fill_one_dpl
+
 #define FILL_GROUP   \
 	FILL_SEQ_WRAPPER \
 	FILL_STD_WRAPPER \
-	FILL_HPX_WRAPPER
+	FILL_HPX_WRAPPER \
+	FILL_ONE_DPL_WRAPPER
 
 FILL_GROUP
