fc198c287d184e13d265684adddb8452526dbd87
Oleg Bilovus <68842946+olegbilovus@users.noreply.github.com>
2025-05-02 18:07:35 +0200

    Add oneDPL support for inclusive_scan benchmarks

--- include/pstl/benchmarks/inclusive_scan/group.h
+++ include/pstl/benchmarks/inclusive_scan/group.h
@@ -10,6 +10,10 @@
 #include "inclusive_scan_hpx.h"
 #endif
 
+#ifdef PSTL_BENCH_USE_ONE_DPL
+#include "inclusive_scan_one_dpl.h"
+#endif
+
 //region inclusive_scan_std
 template<class Policy>
 static void inclusive_scan_std_wrapper(benchmark::State & state)
@@ -49,9 +53,31 @@ static void inclusive_scan_hpx_wrapper(benchmark::State & state)
 #endif
 //endregion inclusive_scan_hpx
 
+//region inclusive_scan_one_dpl
+#ifdef PSTL_BENCH_USE_ONE_DPL
+template<class Policy>
+static void inclusive_scan_one_dpl_wrapper(benchmark::State & state)
+{
+	benchmark_inclusive_scan::benchmark_wrapper<Policy>(state, benchmark_inclusive_scan::inclusive_one_dpl);
+}
+
+/*
+the std policy is just a placeholder, it will use oneapi::dpl::execution::dpcpp_default when executing the algorithm. 
+Check the algorithm implementation.
+*/
+#define INCLUSIVE_SCAN_ONE_DPL_WRAPPER                                                               \
+	BENCHMARK_TEMPLATE1(inclusive_scan_one_dpl_wrapper, std::execution::parallel_unsequenced_policy) \
+	    ->Name(PSTL_BENCH_BENCHMARK_NAME("oneDPL::inclusive_scan"))                          \
+	    ->PSTL_BENCH_BENCHMARK_PARAMETERS
+#else
+#define INCLUSIVE_SCAN_ONE_DPL_WRAPPER
+#endif
+//endregion inclusive_scan_one_dpl
+
 #define INCLUSIVE_SCAN_GROUP   \
 	INCLUSIVE_SCAN_SEQ_WRAPPER \
 	INCLUSIVE_SCAN_STD_WRAPPER \
-	INCLUSIVE_SCAN_HPX_WRAPPER
+	INCLUSIVE_SCAN_HPX_WRAPPER \
+	INCLUSIVE_SCAN_ONE_DPL_WRAPPER
 
 INCLUSIVE_SCAN_GROUP
