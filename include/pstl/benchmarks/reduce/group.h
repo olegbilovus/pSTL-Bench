74f65c0696fc7e3877a6d0f037b6fe8f23b3cdcc
Oleg Bilovus <68842946+olegbilovus@users.noreply.github.com>
2025-05-02 18:13:59 +0200

    Add oneDPL support for reduce benchmarks

--- include/pstl/benchmarks/reduce/group.h
+++ include/pstl/benchmarks/reduce/group.h
@@ -14,6 +14,10 @@
 #include "reduce_hpx.h"
 #endif
 
+#ifdef PSTL_BENCH_USE_ONE_DPL
+#include "reduce_one_dpl.h"
+#endif
+
 //region reduce_std
 template<class Policy>
 static void reduce_std_wrapper(benchmark::State & state)
@@ -70,10 +74,32 @@ static void reduce_hpx_wrapper(benchmark::State & state)
 #endif
 //endregion reduce_hpx
 
+//region reduce_one_dpl
+#ifdef PSTL_BENCH_USE_ONE_DPL
+template<class Policy>
+static void reduce_one_dpl_wrapper(benchmark::State & state)
+{
+	benchmark_reduce::benchmark_wrapper<Policy>(state, benchmark_reduce::reduce_one_dpl);
+}
+
+/*
+the std policy is just a placeholder, it will use oneapi::dpl::execution::dpcpp_default when executing the algorithm. 
+Check the algorithm implementation.
+*/
+#define REDUCE_ONE_DPL_WRAPPER                                                               \
+	BENCHMARK_TEMPLATE1(reduce_one_dpl_wrapper, std::execution::parallel_unsequenced_policy) \
+	    ->Name(PSTL_BENCH_BENCHMARK_NAME("oneDPL::reduce"))                                  \
+	    ->PSTL_BENCH_BENCHMARK_PARAMETERS
+#else
+#define REDUCE_ONE_DPL_WRAPPER
+#endif
+//endregion reduce_one_dpl
+
 #define REDUCE_GROUP   \
 	REDUCE_SEQ_WRAPPER \
 	REDUCE_STD_WRAPPER \
 	REDUCE_GNU_WRAPPER \
-	REDUCE_HPX_WRAPPER
+	REDUCE_HPX_WRAPPER \
+	REDUCE_ONE_DPL_WRAPPER
 
 REDUCE_GROUP
